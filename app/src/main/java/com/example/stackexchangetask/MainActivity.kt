416019e9352cc7414947e00e04db130174c3dad1
its-me-debk007 <debashish.joy@gmail.com>
2023-05-09 09:05:33 +0530

    added viewmodel to complete integrating home api call

--- app/src/main/java/com/example/stackexchangetask/MainActivity.kt
+++ app/src/main/java/com/example/stackexchangetask/MainActivity.kt
@@ -1,20 +1,25 @@
 package com.example.stackexchangetask
 
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.stackexchangetask.databinding.ActivityMainBinding
+import com.example.stackexchangetask.presentation.viewmodel.HomeViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private val viewModel: HomeViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
@@ -25,11 +30,11 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+//        val navController = findNavController(R.id.nav_host_fragment_content_main)
+//        appBarConfiguration = AppBarConfiguration(navController.graph)
+//        setupActionBarWithNavController(navController, appBarConfiguration)
 
-
+        Log.d("VIEWMODEL", viewModel.questions.value.errorMsg.toString())
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
